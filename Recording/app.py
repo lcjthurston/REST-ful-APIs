6f02626835e198ac6457360b0de18a5e05503808
Luke Thurston <lcjthurston@gmail.com>
2025-02-21 15:20:08 -0600

    temporary changes

--- Recording/app.py
+++ Recording/app.py
@@ -15,9 +15,13 @@ stores = [
     }
 ]
 
-@app.get("/store") # http://127.0.0.1:5000/store
+# Endpoints that return data when client requests it
+
+
+@app.get("/store")  # The full path is http://127.0.0.1:5000/store
 def get_stores():
     return {"stores": stores}
+
 
 @app.post("/store")
 def create_store():
@@ -26,12 +30,14 @@ def create_store():
     stores.append(new_store)
     return new_store, 201
 
+
 @app.post("/store/<string:name>/item")
 def create_item(name):
     request_data = request.get_json()
     for store in stores:
         if store["name"] == name:
-            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            new_item = {
+                "name": request_data["name"], "price": request_data["price"]}
             store["items"].append(new_item)
             return new_item, 201
     return {"message": "Store not found"}, 404
@@ -43,6 +49,7 @@ def get_store(name):
         if store["name"] == name:
             return store
     return {"message": "Store not found"}, 404
+
 
 @app.get("/store/<string:name>/item")
 def get_item_in_store(name):
